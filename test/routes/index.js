492095983568b9f0fdce57301efd0e6919f98115
sethvoler <353619470@qq.com>
2017-10-13 21:13:00 +0800

    完成登录注册注销功能

---
+++ test/routes/index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', {});
+});
+
+router.get('/home', function(req, res, next) {
+    console.log(req.session.user);
+  if(req.session.user==undefined){
+    res.redirect('/');
+  }else{
+    res.render('home', {user:req.session.user});
+  }
+});
+
+// register page
+router.get('/register', function(req, res, next) {
+  res.render('register', {});
+});
+
+//注销
+router.get('/logout', function(req, res, next) {
+  //req.session.user = undefined;
+  req.session.destroy((err)=>{
+    if(err){
+        console.log(err);
+    }else{
+        res.redirect('/');
+    }
+  });
+});
+
+module.exports = router;
